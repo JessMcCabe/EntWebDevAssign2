7167dc10141d4d544e6878116298f6fcd862bda8
Jessica McCabe <mccabejessica88@gmail.com>
2024-04-28 16:54:43 +0100

    Update to dark theme on review page

--- src/components/tvShowReview/index.tsx
+++ src/components/tvShowReview/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Review } from "../../types/interfaces";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
 
 const TVShowReview: React.FC<Review> =  (props) => {
+
+  
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
   return (
     <>
+     <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <p>Review By: {props.author} </p>
       <p>{props.content} </p>
+      </ThemeProvider>
     </>
   );
 };
